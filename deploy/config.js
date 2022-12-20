fa076fc462c0cfb2bdc884a948b3219442d01457
huangruilin <1310145384@qq.com>
2022-12-20 21:53:02 +0800

    feat:添加微信小程序笔记

--- deploy/config.js
+++ deploy/config.js
@@ -1,12 +1,12 @@
 const server = {
-	host: '124.221.155.63', // 服务器ip
-	port: '22', // 端口一般默认22
-	username: 'root', // 用户名
-	password: 'huang211314.0', // 密码
-	pathNmae: 'coderh/Coderh_share', // 上传到服务器的位置
-	locaPath: '../build/', // 本地打包文件的位置
-};
+  host: '124.221.155.63', // 服务器ip
+  port: '22', // 端口一般默认22
+  username: 'root', // 用户名
+  password: 'huang211314.0', // 密码
+  pathNmae: 'coderh/Coderh_share', // 上传到服务器的位置
+  locaPath: '../build/', // 本地打包文件的位置
+}
 
 module.exports = {
-	server,
-};
+  server,
+}
