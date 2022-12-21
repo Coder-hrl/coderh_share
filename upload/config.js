bff6b0b15947598c8bc7da900d0b5ed887bd9f01
huangrl <1310145384@qq.com>
2022-12-21 17:52:49 +0800

    feat():添加新的upload程序

---
+++ upload/config.js
@@ -0,0 +1,18 @@
+module.exports = Object.freeze({
+	development: {
+		//测试
+		SERVER_PATH: '124.221.155.63', // ssh地址 服务器地址
+		SSH_USER: 'root', // ssh 用户名
+		//方式一 用秘钥登录服务器(推荐), private 本机私钥文件地址(需要在服务器用户目录 一般是 /root/.ssh/authorized_keys 配置公钥 并该文件权限为 600, (.ssh文件夹一般默认隐藏)
+		// PRIVATE_KEY: 'C:/Users/Html5/.ssh/id_rsa',
+		PASSWORD: 'huang211314.0', //方式二 用密码连接服务器
+		PATH: '/root/coderh/Coderh_share', // 需要上传的服务器目录地址 如 /usr/local/nginx/html
+	},
+	production: {
+		//正式
+		SERVER_PATH: '',
+		SSH_USER: 'root',
+		PRIVATE_KEY: '',
+		PATH: '/test/html',
+	},
+});
